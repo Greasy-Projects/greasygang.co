9e192ec7cff975f22fc4b18c6581bfbd051b902e
veryCrunchy <verycrunchydev@gmail.com>
2024-02-01 05:16:20 +0100

    feat: homepage

---
+++ uno.config.ts
@@ -0,0 +1,47 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
+
+export default defineConfig({
+  presets: [
+    presetUno({}),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        IBM: "IBM Plex Sans",
+      },
+    }),
+  ],
+  shortcuts: [
+    // ...
+  ],
+  theme: {
+    breakpoints: {
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1536px",
+      "2xl": "1536px",
+    },
+    colors: {
+      home: {
+        button: {
+          "": "#ff5454",
+          hovered: "#bd3535",
+        },
+      },
+    },
+  },
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+});
