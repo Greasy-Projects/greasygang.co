e5ee655eb3df691fafcaa4ce315a341ff0630306
veryCrunchy <verycrunchydev@gmail.com>
2024-03-21 01:50:55 +0100

    feat: mc under construction

---
+++ pages/minecraft.vue
@@ -0,0 +1,12 @@
+<template>
+	<main>
+		<NuxtImg
+			class="absolute -z-100 size-full op-80 overflow-hidden object-cover"
+			src="GoopBGTransparent.png"
+		></NuxtImg>
+		<div class="items-center flex h-screen justify-center flex-col">
+			<NuxtImg class="w-6/7 max-w-200" src="greasycraft.png"></NuxtImg>
+			<NuxtImg class="w-6/7 max-w-200" src="mc-under-construction.png"></NuxtImg>
+		</div>
+	</main>
+</template>
