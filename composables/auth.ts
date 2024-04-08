7259e5b9a6750edc5b34f31174e5d43c0063929c
veryCrunchy <verycrunchydev@gmail.com>
2024-04-08 22:42:42 +0200

    feat: typed user attrs

--- composables/auth.ts
+++ composables/auth.ts
@@ -1,19 +1,6 @@
-import type { User } from "lucia";
+import type { GetMeQuery } from "#gql";
 
 export const useUser = () => {
-	const user = useState<User | null>("user", () => null);
+	const user = useAttrs().user as GetMeQuery | null;
 	return user;
-};
-
-export const useAuthenticatedUser = () => {
-	const user = useUser();
-	return computed(() => {
-		const userValue = unref(user);
-		if (!userValue) {
-			throw createError(
-				"useAuthenticatedUser() can only be used in protected pages"
-			);
-		}
-		return userValue;
-	});
 };
