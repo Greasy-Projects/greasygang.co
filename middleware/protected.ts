c2cf2a4ead1471225016c507d475faed036a107a
veryCrunchy <verycrunchydev@gmail.com>
2024-04-24 21:39:27 +0200

    fix: protected middleware

--- middleware/protected.ts
+++ middleware/protected.ts
@@ -1,10 +1,8 @@
 export default defineNuxtRouteMiddleware(async m => {
 	const token = useCookie("auth_session").value;
-	//TODO: validate token and handle accordingly
 	const config = useRuntimeConfig().public;
 	const api = new URL(config.apiBase);
 	api.pathname = "/token/validate";
-	console.log(m.fullPath);
 
 	if (token) {
 		const validate = await useFetch(api.toString(), {
@@ -18,11 +16,7 @@ export default defineNuxtRouteMiddleware(async m => {
 	api.pathname = "/login/twitch";
 	api.searchParams.set("token_callback", config.callbackUrl);
 	api.searchParams.set("redirect", m.fullPath);
-	//TODO: allow dynamic scopes depending on the pages needs
-	api.searchParams.set(
-		"scopes",
-		"bits:read channel:read:editors channel:read:redemptions channel:read:subscriptions user:read:email"
-	);
+	api.searchParams.set("scopes", "user:read:email");
 	return navigateTo(api.toString(), {
 		external: true,
 	});
